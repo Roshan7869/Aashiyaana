bb1040db0b32e9ba79b214e36b215549b184f9c4
Roshan Khatri <134715400+Roshan7869@users.noreply.github.com>
2025-05-11 11:48:24 +0530

    Remove CategorySection

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import CategorySection from '../components/CategorySection';
 import FilterBar from '../components/FilterBar';
 import ListingGrid from '../components/ListingGrid';
 import InspirationSection from '../components/InspirationSection';
@@ -7,7 +6,6 @@ import InspirationSection from '../components/InspirationSection';
 const HomePage: React.FC = () => {
   return (
     <div className="pt-4 pb-12">
-      <CategorySection />
       <FilterBar />
       <ListingGrid />
       <InspirationSection />
